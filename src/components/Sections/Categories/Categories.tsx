edec7884d1dd827e85e4da5b2dfbe8dbd5f729ba
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-31 20:11:56 +0200

    ADD - Media to categories

--- src/components/Sections/Categories/Categories.tsx
+++ src/components/Sections/Categories/Categories.tsx
@@ -1,17 +1,22 @@
 import { Card, CardContent } from '@/components/ui/card'
-import React from 'react'
+import sofas from '@/../public/sofas.webp'
+import tables from '@/../public/table.webp'
+import chairs from '@/../public/chairs.webp'
+import beds from '@/../public/beds.webp'
+import Image from 'next/image'
+
 
 export default function Categories() {
   return (
-    <section className="py-12 px-8 bg-gray-100 ">
+    <section className="py-12 md:py-32 px-8 bg-white ">
       <h2 className="text-2xl font-semibold mb-4">Shop by Category</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         <Card>
-          <img
+          <Image
             alt="Sofas"
             className="w-full h-48 object-cover"
             height="200"
-            src="/placeholder.svg"
+            src={sofas}
             style={{
               aspectRatio: "200/200",
               objectFit: "cover",
@@ -23,11 +28,11 @@ export default function Categories() {
           </CardContent>
         </Card>
         <Card>
-          <img
+          <Image
             alt="Tables"
             className="w-full h-48 object-cover"
             height="200"
-            src="/placeholder.svg"
+            src={tables}
             style={{
               aspectRatio: "200/200",
               objectFit: "cover",
@@ -39,11 +44,11 @@ export default function Categories() {
           </CardContent>
         </Card>
         <Card>
-          <img
+          <Image
             alt="Chairs"
             className="w-full h-48 object-cover"
             height="200"
-            src="/placeholder.svg"
+            src={chairs}
             style={{
               aspectRatio: "200/200",
               objectFit: "cover",
@@ -55,11 +60,11 @@ export default function Categories() {
           </CardContent>
         </Card>
         <Card>
-          <img
+          <Image
             alt="Beds"
             className="w-full h-48 object-cover"
             height="200"
-            src="/placeholder.svg"
+            src={beds}
             style={{
               aspectRatio: "200/200",
               objectFit: "cover",
