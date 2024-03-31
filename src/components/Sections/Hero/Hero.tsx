3d86127755d6e15a76def53d32725b1b009cf224
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-31 19:36:02 +0200

    REFACTOR - Heros

---
+++ src/components/Sections/Hero/Hero.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import heroPhoto from '@/../public/hero.webp'
+
+export default function Hero() {
+  return (
+    <section className="relative h-[500px]">
+      <Image
+        alt="Living room setup"
+        className="absolute  inset-0 object-cover w-full h-full"
+        height="500"
+        src={heroPhoto}
+        style={{
+          aspectRatio: "1000/500",
+          objectFit: "cover",
+        }}
+        width="1000"
+      />
+      <div className="absolute inset-0 backdrop:blur-sm bg-opacity-55 bg-black flex py-32 justify-center text-center">
+        <div className="space-y-2">
+          <h1 className="text-3xl md:text-8xl font-bold text-white">Muebles el dormitorio</h1>
+          <p className="text-lg md:text-3xl text-white">Muebles artesanales en Pamplona</p>
+        </div>
+      </div>
+    </section>
+  )
+}
