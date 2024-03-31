ed265a86da7a2cf21f06039df4aa839c413485cc
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-31 20:12:20 +0200

    UPDATE - Home page

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -1,10 +1,10 @@
 import { CardContent, Card } from "@/components/ui/card"
 import Image from "next/image"
 import Header from "./Header/Header"
-import { FacebookIcon, InstagramIcon, TwitterIcon } from "./Icons/Icons"
 import Hero from "./Sections/Hero/Hero"
 import Furnitures from "./Sections/Furnitures/Furnitures"
 import Categories from "./Sections/Categories/Categories"
+import Footer from "./Footer/Footer"
 
 export function HomePage() {
   return (
@@ -15,14 +15,7 @@ export function HomePage() {
         <Furnitures />
         <Categories />
       </main>
-      <footer className="flex items-center justify-between px-8 py-4 bg-white ">
-        <div className="flex items-center space-x-4">
-          <FacebookIcon className="h-6 w-6" />
-          <TwitterIcon className="h-6 w-6" />
-          <InstagramIcon className="h-6 w-6" />
-        </div>
-        <div className="text-sm text-gray-500 ">© 2024 Furniture Shop. All rights reserved.</div>
-      </footer>
+      <Footer />
     </div>
   )
 }
