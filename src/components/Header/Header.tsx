e22c463da8770bbd8e507dd61106745a31b826a7
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-03-31 19:35:39 +0200

    REFACTOR - Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
+import { HomeIcon } from "../Icons/Icons"
 export default function Header() {
   return (
-    <header className="flex items-center bg-brown-100 justify-between px-8 py-4 bg-white ">
+    <header className="flex items-center  justify-between px-8 py-4 bg-transparent ">
       <Link className="flex items-center" href="#">
         <HomeIcon className="h-6 w-6" />
-        <span className="ml-2 text-lg font-semibold">Furniture Shop</span>
+        <span className="ml-2 text-lg font-semibold">El Dormitorio</span>
       </Link>
       <nav className="flex gap-4">
         <Link className="text-sm font-medium hover:underline" href="#">
